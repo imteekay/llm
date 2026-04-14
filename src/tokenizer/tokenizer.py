5dd1eb89720e949b182d415236ca0b8202093ad7
TK <leandro.kinoshita@quintoandar.com.br>
2026-04-14 07:59:06 -0300

    *

--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+import re
 
+# === open the text file ===
 _DATA = Path(__file__).resolve().parent / "data" / "the-verdict.txt"
 
 with open(_DATA, "r", encoding="utf-8") as file:
@@ -7,3 +9,19 @@ with open(_DATA, "r", encoding="utf-8") as file:
 
 print(len(raw_text))
 print(raw_text[:1000])
+# === // ===
+
+# === tokenize the test text ===
+text = "Hello, world. Is this-- a test?"
+result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+print(result)
+
+tokenized_text = [item for item in result if item.strip()]
+print(tokenized_text)
+# === // ===
+
+# === preprocess the the verdict text ===
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item for item in preprocessed if item.strip()]
+print(preprocessed[:30])
+# === // ===
