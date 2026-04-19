0d905c7068ad564f42b6761135f1dfc934e89054
TK <leandro.kinoshita@quintoandar.com.br>
2026-04-19 17:37:46 -0300

    *

---
+++ src/attention/simple_self_attention.py
@@ -0,0 +1,38 @@
+import torch
+
+attn_scores = torch.empty(6, 6)
+
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55]] # step     (x^6)
+)
+
+# === compute attention scores through loop ===
+for i, x_i in enumerate(inputs):
+    for j, x_j in enumerate(inputs):
+        attn_scores[i, j] = torch.dot(x_i, x_j)
+
+print("Attention scores through loop:\n", attn_scores)
+# === // ===
+
+# === compute attention scores through matrix multiplication ===
+attn_scores = inputs @ inputs.T
+
+print("Attention scores through matrix multiplication:\n", attn_scores)
+# === // ===
+
+# === compute attention weights through softmax ===
+attn_weights = torch.softmax(attn_scores, dim=-1)
+
+print("Attention weights through softmax:\n", attn_weights)
+# === // ===
+
+# === compute context vectors ===
+context_vectors = attn_weights @ inputs
+
+print("Context vectors:\n", context_vectors)
+# === // ===
