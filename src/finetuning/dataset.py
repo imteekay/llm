02022d1dcd6f1d2485ac867b8be604c830abb9a2
TK <leandro.kinoshita@quintoandar.com.br>
2026-05-02 15:39:48 -0300

    *

---
+++ src/finetuning/dataset.py
@@ -0,0 +1,82 @@
+import requests
+import zipfile
+import os
+import pandas as pd
+import torch
+
+from pathlib import Path
+
+torch.manual_seed(123)
+
+url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
+zip_path = "sms_spam_collection.zip"
+extracted_path = "src/data/sms_spam_collection"
+data_file_path = Path(extracted_path) / "SMSSpamCollection.tsv"
+
+
+def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
+    if data_file_path.exists():
+        print(f"{data_file_path} already exists. Skipping download and extraction.")
+        return
+
+    # Downloading the file
+    response = requests.get(url, stream=True, timeout=60)
+    response.raise_for_status()
+    with open(zip_path, "wb") as out_file:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                out_file.write(chunk)
+
+    # Unzipping the file
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extracted_path)
+
+    # Add .tsv file extension
+    original_file_path = Path(extracted_path) / "SMSSpamCollection"
+    os.rename(original_file_path, data_file_path)
+    print(f"File downloaded and saved as {data_file_path}")
+
+
+try:
+    download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
+except (requests.exceptions.RequestException, TimeoutError) as e:
+    print(f"Primary URL failed: {e}. Trying backup URL...")
+    url = "https://f001.backblazeb2.com/file/LLMs-from-scratch/sms%2Bspam%2Bcollection.zip"
+    download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
+
+df = pd.read_csv(data_file_path, sep="\t", header=None, names=["Label", "Text"])
+print(df.head())
+print(df["Label"].value_counts())
+
+def create_balanced_dataset(df):
+    num_spam = df[df["Label"] == "spam"].shape[0] # 747
+    ham_subset = df[df["Label"] == "ham"].sample(
+        num_spam, random_state=123
+    )
+    balanced_df = pd.concat([
+        ham_subset, df[df["Label"] == "spam"]
+    ])
+    return balanced_df
+
+balanced_df = create_balanced_dataset(df)
+print(balanced_df["Label"].value_counts())
+
+# One-hot encode the labels
+balanced_df["Label"] = balanced_df["Label"].map({"ham": 0, "spam": 1})
+
+def random_split(df, train_frac, validation_frac):
+    df = df.sample(frac=1, random_state=123).reset_index(drop=True)
+    train_end = int(len(df) * train_frac)
+    validation_end = train_end + int(len(df) * validation_frac)
+
+    train_df = df[:train_end]
+    validation_df = df[train_end:validation_end]
+    test_df = df[validation_end:]
+
+    return train_df, validation_df, test_df
+
+train_df, validation_df, test_df = random_split(balanced_df, 0.7, 0.1)
+
+train_df.to_csv("train.csv", index=None)
+validation_df.to_csv("validation.csv", index=None)
+test_df.to_csv("test.csv", index=None)
