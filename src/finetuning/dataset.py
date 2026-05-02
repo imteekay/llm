0d0513235bf63dcaaa6401a886bbf7542ac1121c
TK <leandro.kinoshita@quintoandar.com.br>
2026-05-02 16:39:00 -0300

    *

--- src/finetuning/dataset.py
+++ src/finetuning/dataset.py
@@ -6,14 +6,15 @@ import torch
 import tiktoken
 
 from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
 
 torch.manual_seed(123)
 
+# === download and unzip the spam data ===
 url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
 zip_path = "sms_spam_collection.zip"
 extracted_path = "src/data/sms_spam_collection"
 data_file_path = Path(extracted_path) / "SMSSpamCollection.tsv"
-
 
 def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
     if data_file_path.exists():
@@ -37,7 +38,6 @@ def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
     os.rename(original_file_path, data_file_path)
     print(f"File downloaded and saved as {data_file_path}")
 
-
 try:
     download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
 except (requests.exceptions.RequestException, TimeoutError) as e:
@@ -48,7 +48,9 @@ except (requests.exceptions.RequestException, TimeoutError) as e:
 df = pd.read_csv(data_file_path, sep="\t", header=None, names=["Label", "Text"])
 print(df.head())
 print(df["Label"].value_counts())
+# === // ===
 
+# === create a balanced dataset ===
 def create_balanced_dataset(df):
     num_spam = df[df["Label"] == "spam"].shape[0] # 747
     ham_subset = df[df["Label"] == "ham"].sample(
@@ -64,7 +66,9 @@ print(balanced_df["Label"].value_counts())
 
 # One-hot encode the labels
 balanced_df["Label"] = balanced_df["Label"].map({"ham": 0, "spam": 1})
+# === // ===
 
+# === split the dataset into train, validation, and test sets ===
 def random_split(df, train_frac, validation_frac):
     df = df.sample(frac=1, random_state=123).reset_index(drop=True)
     train_end = int(len(df) * train_frac)
@@ -78,9 +82,106 @@ def random_split(df, train_frac, validation_frac):
 
 train_df, validation_df, test_df = random_split(balanced_df, 0.7, 0.1)
 
-train_df.to_csv("train.csv", index=None)
-validation_df.to_csv("validation.csv", index=None)
-test_df.to_csv("test.csv", index=None)
+train_path = "src/finetuning/dataset/train.csv"
+validation_path = "src/finetuning/dataset/validation.csv"
+test_path = "src/finetuning/dataset/test.csv"
+
+if not os.path.exists(train_path):
+    train_df.to_csv(train_path, index=None)
+
+if not os.path.exists(validation_path):
+    validation_df.to_csv(validation_path, index=None)
+
+if not os.path.exists(test_path):
+    test_df.to_csv(test_path, index=None)
+# === // ===
+
+# === create a dataset class ===
+class SpamDataset(Dataset):
+    def __init__(self, csv_file, tokenizer, max_length=None, pad_token_id=50256):
+        self.data = pd.read_csv(csv_file)
+        self.encoded_texts = [
+            tokenizer.encode(text) for text in self.data["Text"]
+        ]
+
+        if max_length is None:
+            self.max_length = self._longest_encoded_length()
+        else:
+            self.max_length = max_length
+            self.encoded_texts = [
+                encoded_text[:self.max_length]
+                for encoded_text in self.encoded_texts
+            ]
+
+        self.encoded_texts = [
+            encoded_text + [pad_token_id] * 
+            (self.max_length - len(encoded_text))
+            for encoded_text in self.encoded_texts
+        ]
+
+    def __getitem__(self, index):
+        encoded = self.encoded_texts[index]
+        label = self.data.iloc[index]["Label"]
+        return (
+            torch.tensor(encoded, dtype=torch.long),
+            torch.tensor(label, dtype=torch.long)
+        )
+
+    def __len__(self):
+        return len(self.data)
+
+    def _longest_encoded_length(self):
+        max_length = 0
+        for encoded_text in self.encoded_texts:
+            encoded_length = len(encoded_text)
+            if encoded_length > max_length:
+                max_length = encoded_length
+        return max_length
 
 tokenizer = tiktoken.get_encoding("gpt2")
-print(tokenizer.encode("<|endoftext|>", allowed_special={"<|endoftext|>"}))
+
+train_dataset = SpamDataset(
+    csv_file=train_path,
+    max_length=None,
+    tokenizer=tokenizer
+)
+
+val_dataset = SpamDataset(
+    csv_file=validation_path,
+    max_length=train_dataset.max_length,
+    tokenizer=tokenizer
+)
+
+test_dataset = SpamDataset(
+    csv_file=test_path,
+    max_length=train_dataset.max_length,
+    tokenizer=tokenizer
+)
+# === // ===
+
+# === create data loaders ===
+num_workers = 0
+batch_size = 8
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_workers,
+    drop_last=True,
+)
+
+val_loader = DataLoader(
+    dataset=val_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    drop_last=False,
+)
+
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    drop_last=False,
+)
+# === // ===
